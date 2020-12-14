65e950bb56d770a30e8f5d6b6f281cddf0ae1000
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-12-14 12:56:10 +0530

    Update bot_alive.py

--- userbot/modules/assistant/bot_alive.py
+++ userbot/modules/assistant/bot_alive.py
@@ -5,8 +5,8 @@ from userbot import ALIVE_NAME, bot
 currentversion = "4.9"
 
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Black Lightning"
-PM_IMG = "https://telegra.ph/file/7f72b0ea1893e84028298.mp4"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Javes"
+PM_IMG = "https://telegra.ph/file/2f2b8d40e3f2fa4acdc8f.mp4"
 pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
 pm_caption += "➥ **SYSTEMS STATS**\n"
 pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
@@ -18,7 +18,7 @@ pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
 pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
 pm_caption += "➥ **License** : [GNU General Public License v3.0](https://github.com/KeinShin/Black-Lightning/blob/master/LICENSE)\n"
 pm_caption += "➥ **Copyright** : By [Javes](GitHub.com/Javes786)\n"
-pm_caption += "[Assistant By Javes 🇮🇳](https://telegra.ph/file/b233f8b6332fbeb3f61dc.mp4)"
+pm_caption += "[Assistant By Javes 🇮🇳](https://telegra.ph/file/2f2b8d40e3f2fa4acdc8f.mp4)"
 
 
 @tgbot.on(events.NewMessage(pattern="^/alive", func=lambda e: e.sender_id == bot.uid))
